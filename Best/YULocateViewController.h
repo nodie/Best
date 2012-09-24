db25491a9f9c9e38c65dcc76cc74ae54802cfcce
nodie <landyer@163.com>
2012-09-24 09:44:19 +0800

    Best for you
    初始化导入

---
+++ Best/YULocateViewController.h
@@ -0,0 +1,13 @@
+//
+//  YULocateViewController.h
+//  Best
+//
+//  Created by 於 琦峰 on 12-09-23.
+//  Copyright (c) 2012年 於 琦峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YULocateViewController : UITableViewController
+@property (strong, nonatomic) NSArray *provinces;
+@end
